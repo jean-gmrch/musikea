7345d6a35dc5e3cf86b72441fd45db54ab7c0c6e
Jean Guyomarc'h <jeang120@outlook.fr>
2025-02-07 15:43:40 +0100

    update docker compose

--- backend/main.py
+++ backend/main.py
@@ -15,11 +15,11 @@ settings = Settings()
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/api/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/api/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
