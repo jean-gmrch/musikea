2e76bfc912ae93cfa6859af584266db757496968
Jean Guyomarc'h <jeang120@outlook.fr>
2025-02-10 14:56:05 +0100

    Recherche back ok

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,14 @@
 from typing import Union
-import os
-import requests
-import time
-import random
-from dotenv import load_dotenv
+
 from fastapi import FastAPI
-from pydantic_settings import BaseSettings
+
+from .config import settings
+
+from .spotify import router as spotify_router
 
 
-class Settings(BaseSettings):
-    SPOTIFY_CLIENT_ID: str
-    SPOTIFY_CLIENT_SECRET: str
-    SPOTIFY_API_URL: str
-    SPOTIFY_AUTH_URL: str
-
-
-settings = Settings()
 app = FastAPI()
-
-
-@app.get("/api/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/api/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(spotify_router)
 
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
